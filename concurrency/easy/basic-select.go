dabee3c108cde072ba138342e5f3890c16e69f29
Alexander <lisitsyn1994digo@gmail.com>
2025-08-10 14:37:24 +0500

    1 and 2 tasks

--- concurrency/easy/basic-select.go
+++ concurrency/easy/basic-select.go
@@ -8,6 +8,13 @@ import (
 
 // Что выведет?
 
+// Ответ:
+
+// waited for 1 sec
+// waited for 1 sec
+// третий раз наверное не успеет выполниться, так как контекст закончится раньше, потому что опереации, которые не входят в select тоже займут какое-то небольшое время и третья "wait for 1 sec" не успеет и будет:
+// deadline exceeded
+
 func main() {
 	timeout := 3 * time.Second
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
--- concurrency/easy/filtering-nums.go
+++ concurrency/easy/filtering-nums.go
@@ -2,3 +2,57 @@
 // Вторая горутина читает из A и отправляет в канал B только чётные числа.
 // Третья горутина читает из B и выводит числа.
 // Можно с WaitGroup, можно с <-time.After()
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+var (
+	wg = &sync.WaitGroup{}
+	N  = 100
+)
+
+func gorutine1(n int, A chan<- int) {
+	defer wg.Done()
+	defer close(A)
+
+	for i := 0; i < 10; i++ {
+		rnd := rand.Intn(n) + 1
+		A <- rnd
+	}
+
+}
+
+func gorutine2(A <-chan int, B chan<- int) {
+	defer wg.Done()
+	defer close(B)
+
+	for num := range A {
+		if num%2 == 0 {
+			B <- num
+		}
+	}
+}
+
+func gorutine3(B <-chan int) {
+	defer wg.Done()
+
+	for sorted_nums := range B {
+		fmt.Println(sorted_nums)
+	}
+}
+
+func main() {
+	A := make(chan int)
+	B := make(chan int)
+
+	wg.Add(3)
+	go gorutine1(N, A)
+	go gorutine2(A, B)
+	go gorutine3(B)
+
+	wg.Wait()
+}
